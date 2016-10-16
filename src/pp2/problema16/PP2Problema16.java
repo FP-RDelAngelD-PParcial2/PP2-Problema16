fe11e409f54650883f494a017b94f29db021c75e
PC <PC@192.168.1.70>
2016-10-15 19:34:12 -0500

    16.	Realizar un programa que contenga una función que reciba como parámetros tres números y que devuelva y muestre el mayor de ellos.

--- src/pp2/problema16/PP2Problema16.java
+++ src/pp2/problema16/PP2Problema16.java
@@ -15,13 +15,25 @@ public class PP2Problema16 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        comparaNumeros();
     }
-    public static int solicitarDatos(String d){ //Solicita datos
-        int miDato;
+    public static double solicitarDatos(String d){ //Solicita datos
+        double miDato;
         Scanner entradaEscaner = new Scanner(System.in);
         System.out.print("Introduce el dato " + d);
-        miDato= entradaEscaner.nextInt();
+        miDato= entradaEscaner.nextDouble();
         return miDato;
+    }
+    public static void comparaNumeros(){
+        double a= solicitarDatos("primer numero " + "\n");
+        double b= solicitarDatos("primer numero " + "\n");
+        double c= solicitarDatos("primer numero " + "\n");
+        if (a>b && a>c){
+            System.out.print("El numero mayor es " + a);
+        } else if (b>a && b>c){
+            System.out.print("El numero mayor es " + b);
+        }else if(c>a && c>b){
+            System.out.print("El numero mayor es " + c);
+        }
     }
 }
